a9ae514f108b8a0f8136b57cf0f1471f29f7d935
Jo Sugyeong <dory0107@naver.com>
2015-07-31 20:29:50 +0900

    Update SimpleAdding.rb

--- SimpleAdding.rb
+++ SimpleAdding.rb
@@ -1,10 +1,11 @@
 def SimpleAdding(num)
 
   # code goes here
-  if (num == 0)
-    return 0
-  else
-    return num + SimpleAdding(num - 1)
+  sum = 0
+  while(num > 0)
+    sum = sum + num
+    num = num - 1
   end
+  return sum
          
 end
